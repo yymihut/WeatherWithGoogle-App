57b46451eddfca5c9b2fe6b70f41fd9973ddf01d
Mihut Marius <yymihut@gmail.com>
2020-07-26 11:27:53 +0300

    Updates

---
+++ precache-manifest.f31cb0bbf4983840ec20e7fdbdbeee95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5f2599704399bb5af5214f77173c139",
+    "url": "/WeatherWithGoogle-App/index.html"
+  },
+  {
+    "revision": "7307921ea60a7265596d",
+    "url": "/WeatherWithGoogle-App/static/css/main.271e6029.chunk.css"
+  },
+  {
+    "revision": "f7d814016ed10ebdacbd",
+    "url": "/WeatherWithGoogle-App/static/js/2.61e00f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherWithGoogle-App/static/js/2.61e00f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7307921ea60a7265596d",
+    "url": "/WeatherWithGoogle-App/static/js/main.6a19a33d.chunk.js"
+  },
+  {
+    "revision": "ff50d4a87e66142770b8",
+    "url": "/WeatherWithGoogle-App/static/js/runtime-main.c3061646.js"
+  },
+  {
+    "revision": "b849ea30e66e4ee706d2d97f5062bc77",
+    "url": "/WeatherWithGoogle-App/static/media/background.b849ea30.jpeg"
+  }
+]);
